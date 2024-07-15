e122a7fc852163e1a56088e552be20baf9b413bb
0BooM <149960425+0BooM@users.noreply.github.com>
2024-07-15 22:49:38 +0200

    Create displayBook() which show every users' book on site
    Made function which creates a card then adds content appropriate content for every book added by user. Finally it shows on site

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function Book(title, author, pagesAmount, readStatus) {
   this.title = title;
   this.author = author;
   this.pagesAmount = pagesAmount;
-  this.readStaftus = readStatus;
+  this.readStatus = readStatus;
 }
 
 function addBookToLibrary(title, author, pagesAmount, readStatus) {
@@ -12,6 +12,33 @@ function addBookToLibrary(title, author, pagesAmount, readStatus) {
   myLibrary.push(book);
 }
 
-addBookToLibrary("Mały książe", "Jan Taduesz", 300, "read");
-addBookToLibrary("Wiedzmin", "Piotr Piastowski", 243, "unread");
-console.log(myLibrary[1]);
+addBookToLibrary("Mały Książę", "Antoine de Saint-Exupéry", 300, "Read");
+addBookToLibrary("Wiedźmin", "Andrzej Sapkowski", 243, "Unread");
+addBookToLibrary("Pan Tadeusz", "Adam Mickiewicz", 345, "Unread");
+addBookToLibrary("Kordian", "Juliusz Słowacki", 322, "Read");
+
+function displayBook() {
+  let booksCardList = document.querySelector(".booksList");
+  for (book in myLibrary) {
+    let bookCard = document.createElement("div");
+    let title = document.createElement("h2");
+    let author = document.createElement("h3");
+    let pages = document.createElement("h3");
+    let status = document.createElement("h3");
+
+    title.innerText = "Title: " + myLibrary[book].title;
+    author.innerText = "Author: " + myLibrary[book].author;
+    pages.innerHTML = "Pages: " + myLibrary[book].pagesAmount;
+    status.innerHTML = "Status: " + myLibrary[book].readStatus;
+
+    bookCard.classList.add("bookCard");
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(status);
+
+    booksCardList.appendChild(bookCard);
+  }
+}
+
+displayBook();
