d29303f837aa4f53f9cd96444aea0366d8d9a52f
0BooM <149960425+0BooM@users.noreply.github.com>
2024-07-16 20:34:56 +0200

    Make working validation on the dialog form
    Now you must meet specific criteria to add book to your library. formValidation() makes clear what every input needs and in eventListener for Add Book button in the dialog form is the if statement that adds if criterias are met

--- scripts.js
+++ scripts.js
@@ -56,6 +56,52 @@ closeDialogBtn.addEventListener("click", (e) => {
   dialog.close();
 });
 
+function formValidation() {
+  let title = document.querySelector("#title");
+  let titleSpan = document.querySelector(".titleSpan");
+
+  title.addEventListener("input", (e) => {
+    if (title.value.length >= 10)
+      titleSpan.innerText = "Correct number of characters";
+    else {
+      titleSpan.innerText = "At least 10 characters";
+    }
+  });
+
+  let author = document.querySelector("#author");
+  let authorSpan = document.querySelector(".authorSpan");
+  author.addEventListener("input", (e) => {
+    if (author.value.length >= 5)
+      authorSpan.innerText = "Correct number of characters";
+    else {
+      authorSpan.innerText = "At least 5 characters";
+    }
+  });
+
+  let pages = document.querySelector("#pages");
+  let pagesSpan = document.querySelector(".pagesSpan");
+  pages.addEventListener("input", (e) => {
+    if (+pages.value >= 1) pagesSpan.innerText = "Correct!";
+    else {
+      pagesSpan.innerText = "Book must contain page!";
+    }
+  });
+
+  let bookStatus = document.querySelector("#status");
+  let statusSpan = document.querySelector(".statusSpan");
+  bookStatus.addEventListener("input", (e) => {
+    if (
+      bookStatus.value.toUpperCase() == "READ" ||
+      bookStatus.value.toUpperCase() == "UNREAD"
+    )
+      statusSpan.innerText = "Correct!";
+    else {
+      statusSpan.innerText = "Incorrect! Available statuses: Read/Unread";
+    }
+  });
+}
+formValidation();
+
 let addBookFormBtn = document.querySelector("#addBookForm");
 addBookFormBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -64,47 +110,19 @@ addBookFormBtn.addEventListener("click", (e) => {
   let author = document.querySelector("#author");
   let pages = document.querySelector("#pages");
   let status = document.querySelector("#status");
-
-  addBookToLibrary(title.value, author.value, pages.value, status.value);
-  displayBook();
-  dialog.close();
-
-  title.value = "";
-  author.value = "";
-  pages.value = "";
-  status.value = "";
-});
-
-let title = document.querySelector("#title");
-let titleSpan = document.querySelector(".titleSpan");
-
-title.addEventListener("input", (e) => {
-  if (title.value.length < 10) titleSpan.innerText = "At least 10 characters";
-  else titleSpan.innerText = "Correct number of characters";
-});
-
-let author = document.querySelector("#author");
-let authorSpan = document.querySelector(".authorSpan");
-author.addEventListener("input", (e) => {
-  if (author.value.length < 5) authorSpan.innerText = "At least 5 characters";
-  else authorSpan.innerText = "Correct number of characters";
-});
-
-let pages = document.querySelector("#pages");
-let pagesSpan = document.querySelector(".pagesSpan");
-pages.addEventListener("input", (e) => {
-  if (+pages.value < 1) pagesSpan.innerText = "Book must contain page!";
-  else pagesSpan.innerText = "Correct!";
-});
-
-let bookStatus = document.querySelector("#status");
-let statusSpan = document.querySelector(".statusSpan");
-bookStatus.addEventListener("input", (e) => {
   if (
-    bookStatus.value.toUpperCase() == "READ" ||
-    bookStatus.value.toUpperCase() == "UNREAD"
-  )
-    statusSpan.innerText = "Correct!";
-  else statusSpan.innerText = "Incorrect! Available statuses: Read/Unread";
-  console.log(bookStatus.value);
+    title.value.length >= 10 &&
+    author.value.length >= 5 &&
+    +pages.value >= 1 &&
+    (status.value.toUpperCase() == "READ" ||
+      status.value.toUpperCase() == "UNREAD")
+  ) {
+    addBookToLibrary(title.value, author.value, pages.value, status.value);
+    displayBook();
+    dialog.close();
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    status.value = "";
+  }
 });
